33e3a6f49d963ed62c13834f24277072bc94670c
jaemin <anjm1020@gmail.com>
2022-07-12 17:03:53 +0900

    Feat : updae pull.sh

--- bin/pull.sh
+++ bin/pull.sh
@@ -13,7 +13,16 @@ git reset --hard origin/develop
 git pull origin develop
 
 echo "==========================================="
-echo "2. Build"
+echo "2. Install node"
+echo "==========================================="
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install v16
+
+echo "==========================================="
+echo "3. Build"
 echo "==========================================="
 cd $REPO_DIR
 [ -d build ] && rm -rf build
